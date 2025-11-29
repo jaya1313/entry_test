937313818deb38932624f3c158a03b124a6bd6a5
jaya1313 <jayakarwani4@gmail.com>
2025-11-29 19:33:44 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const box1=document.querySelector(".box1");
+const box2=document.querySelector(".box2");
+const box3=document.querySelector(".box3");
+
+function colorChange(){
+    box1.addEventListener("click",() =>{
+        
+    })
+}
